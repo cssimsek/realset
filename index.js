48de7c427a3db21ac999cdb9989025a05bc7f485
cssimsek <cem@cemsimsek.com>
2018-02-28 01:31:46 +0000

    Beautify JS files

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const RealSet = require("./realset.js");
 (async () => {
     const realSetInstance1 = new RealSet([]);
     const realSetInstance2 = new RealSet([]);
-    for(let i=0;i<10;i+=1){
-        if(i<=5)realSetInstance1.add(i);
-        if(i>=5)realSetInstance2.add(i);
+    for (let i = 0; i < 10; i += 1) {
+        if (i <= 5) realSetInstance1.add(i);
+        if (i >= 5) realSetInstance2.add(i);
     }
     let intersectSet = realSetInstance1.intersect(realSetInstance2);
     let unionSet = realSetInstance1.union(realSetInstance2);
@@ -14,8 +14,7 @@ const RealSet = require("./realset.js");
         "Union": Array.from(unionSet.values())
     }
 })().then(data => {
-    for(let key in data){
+    for (let key in data) {
         console.log(`${key}: ${data[key]}`);
     }
 });
-  
--- realset.js
+++ realset.js
@@ -1,34 +1,32 @@
-module.exports =  class RealSet extends Set {
-	constructor(){
-		super();
-	}
-	union(setB){
+module.exports = class RealSet extends Set {
+    constructor() {
+        super();
+    }
+    union(setB) {
         const unionSet = new RealSet([]);
-        unionSet.refStore=[this,setB];
-        unionSet.refresh = ()=>{
-                return unionSet.refStore[0].union(unionSet.refStore[1]);
+        unionSet.refStore = [this, setB];
+        unionSet.refresh = () => {
+            return unionSet.refStore[0].union(unionSet.refStore[1]);
         }
-        const [larger,smaller] = this.size>setB.size?[this,setB]:[setB,this];
-        for(let itemX of larger){
+        const [larger, smaller] = this.size > setB.size ? [this, setB] : [setB, this];
+        for (let itemX of larger) {
             unionSet.add(itemX);
         }
-        for(let itemY of smaller){
-            if(!larger.has(itemY))unionSet.add(itemY);
+        for (let itemY of smaller) {
+            if (!larger.has(itemY)) unionSet.add(itemY);
         }
         return unionSet;
-	}
-    intersect(setB){
+    }
+    intersect(setB) {
         const intersectSet = new RealSet([]);
-        intersectSet.refStore=[this,setB];
-        intersectSet.refresh = ()=>{
+        intersectSet.refStore = [this, setB];
+        intersectSet.refresh = () => {
             return intersectSet.refStore[0].intersect(intersectSet.refStore[1]);
         }
-        const [larger,smaller] = this.size>setB.size?[this,setB]:[setB,this];
-        for(let item of smaller){
-            if(larger.has(item))intersectSet.add(item);
+        const [larger, smaller] = this.size > setB.size ? [this, setB] : [setB, this];
+        for (let item of smaller) {
+            if (larger.has(item)) intersectSet.add(item);
         }
         return intersectSet;
-	}
+    }
 }
-
-
